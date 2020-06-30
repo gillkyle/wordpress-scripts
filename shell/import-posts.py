db15e938503aa4c9e52553074624f702509ae514
Kyle Gill <kylerobertgill@gmail.com>
2020-06-30 17:21:57 -0600

    refine data from json

--- shell/import-posts.py
+++ shell/import-posts.py
@@ -6,7 +6,7 @@ from csv import reader
 import json
 
 # this script is run from the terminal that has ssh'd into a Local WordPress terminal
-# via "Open Site Shell". It is run with like so:
+# via "Open Site Shell". It is run like so (matching your local machine's path to the clone):
 # $ ./../../../../../kyle/Github/wordpress-scripts/shell/import-posts.py
 
 # You can run system/wp commands like this:
@@ -14,29 +14,22 @@ import json
 
 basePath = "./../../../../../kyle/Github/wordpress-scripts"
 
-# the posts.csv file comes from an online JSON to CSV conversion after running
-# the all-blog-post.graphql query in the GatsbyJS.org wwww site
-# 0 fields_slug
-# 1 fields_excerpt
-# 2 frontmatter_author_id
-# 3 frontmatter_date
-# 4 frontmatter_title
-# 5 frontmatter_canonicalLink
-# 6 frontmatter_publishedAt
-# 7 frontmatter_tags
-# 8 frontmatter_image_id
-# 9 frontmatter_image_relativePath
-# 10 frontmatter_imageTitle
-# 11 rawBody
-
-with open('./../../../../../kyle/Github/wordpress-scripts/posts.json') as f:
+with open('./../../../../../kyle/Github/wordpress-scripts/single-post.json') as f:
     posts = json.load(f)
 
-print()
-
 for post in posts['data']['allMdx']['nodes']:
+    # access and normalize the data from JSON
+    excerpt = post['fields']['excerpt']
+    slug = post['fields']['slug'].replace("/blog/", "")
     author = post['frontmatter']['author']['id'].replace(" ", "")
     title = post['frontmatter']['title']
+    date = post['frontmatter']['date']
+    seo_title = post['frontmatter']['seoTitle']
+    canonical_link = post['frontmatter']['canonicalLink']
+    published_at = post['frontmatter']['publishedAt']
+    content = post['rawBody']
+    tags = ', '.join(post['frontmatter']['tags'])
+
     print(f'Processing post {title}...')
 
     print(f'Searching for author "{author}" in post:')
@@ -45,40 +38,20 @@ for post in posts['data']['allMdx']['nodes']:
         f"awk -v author='{author}' '{{ if($2 == author) {{ print $1 }} }}' './../../../../../kyle/Github/wordpress-scripts/shell/authors.txt'", shell=True, universal_newlines=True).strip()
     print(f'Author ID found for "{author}": {author_id}')
 
-# with open(f'{basePath}/posts.csv', 'r') as read_obj:
-#     # pass the file object to reader() to get the reader object
-#     csv_reader = reader(read_obj)
-#     header = next(csv_reader)
+    # create the post by instantiating the wp command in the shell, passing in the proper variables
+    post_id = subprocess.check_output(
+        f'wp post create --post_author={author_id} --post_date={date} --post_status="publish" --post_title="{title}" --post_excerpt="{excerpt}" --tags_input="{tags}" --published_at="{published_at}" --porcelain', shell=True, universal_newlines=True).strip()
+    print(f'The post id is {post_id}')
 
-#     # Iterate over each row in the csv using reader object
-#     for row in csv_reader:
-#         post_slug = row[0]
-#         post_author = row[2]
-#         post_title = row[4]
-#         post_tags = row[7]
-#         post_raw_body = row[11]
-#         # row variable is a list that represents a row in csv
-#         print(f'Processing post {post_title}...')
-#         # find the author of the post from authors.txt
-#         # txt file created like this: $ wp user list > ./../../../../../kyle/Github/wordpress-scripts/shell/authors.txt
-#         print(f'Searching for author "{post_author}" in post:')
-#         # remove space from name to match username in awk output
-#         # ie "Kyle Mathew" -> "KyleMathews"
-#         author_name = post_author.replace(" ", "")
-
-#         # awk the user list from WP to find the proper author ID as a string
-#         author_id = subprocess.check_output(
-#             f"awk -v author='{author_name}' '{{ if($2 == author) {{ print $1 }} }}' './../../../../../kyle/Github/wordpress-scripts/shell/authors.txt'", shell=True, universal_newlines=True).strip()
-#         print(f'Author ID found for "{post_author}": {author_id}')
-#         # print(author_id == '3')
-#         # print(int(author_id) == 3)
-
-#         # post creation
-#         # os.system(
-#         #     f'wp post create --post_author={author_id} --post_title="{post_title}"')
-
-#         # print(f'{post_slug} {post_author} {post_tags}')
-#         # print(f'{post_raw_body}')
-#         # print("------")
+    # add meta fields that come from ACF
+    if seo_title:
+        os.system(f'wp post meta set {post_id} seo_title "{seo_title}"')
+    if excerpt:
+        os.system(f'wp post meta set {post_id} text "{excerpt}"')
+    if published_at:
+        os.system(f'wp post meta set {post_id} published_at "{published_at}"')
+    if canonical_link:
+        os.system(
+            f'wp post meta set {post_id} canonical_link "{canonical_link}"')
 
 print("done")
